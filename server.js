da7f66820ff3ef92bb3ccb7bd30d71a21b9eb2b6
Aaron Matthews <dan.interesting@gmail.com>
2020-03-13 20:56:55 +1100

    -cleaning

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 	var db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
+//db.on('error', console.error.bind(console, 'connection error:'));
 
 /*db.once('open', function() {
 		console.log("Connection Successful!");
@@ -56,90 +56,119 @@ var newHour = function(id) {
 //searchAll();
 
 
-const productivityObj = {
-	"Very Unproductive": 0,
-	"Unproductive:": 0,
-	"Neutral": 0,
-	"Productive": 0,
-	"Very Productive": 0
-};
-
 app.get('/rtime', function(req, res){
   rTimeTest();
 });
 
 function getTime(){
-	var endDate = moment();
-	//var beginDate = new Date(moment());
-	//beginDate.setDate(beginDate.getDate() - 1);
-	//console.log(endDate, beginDate);
-	var offset = endDate.utcOffset();
-	endDate = endDate.add(offset, 'minutes');
-	var time = endDate.toISOString();
-	console.log(time);
-	return;
-	beginDate.toISOString().truncate; //
-	endDate.toISOString().truncate;
-	var fetchString = `restrict_begin=${beginDate}&restrict_end=${endDate}`
+	var date = moment().subtract(1, 'days');
+	var offset = date.utcOffset();
+	date = date.add(offset, 'minutes')
+		.toISOString()
+		.slice(0, 10);
+	//console.log(beginDate, endDate);
+	var fetchString = `restrict_begin=${date}&restrict_end=${date}`
+	console.log(fetchString);
 	return fetchString;
-	console.log(ISOtimeNow);
+
 }
 
 getTime();
 
 function rTimeTest() {
-	//select today's date and convert it to a format the API can read
-	var today = getTime();
-	axios.get('https://www.rescuetime.com/anapi/data?key=B63lvEkh_mK25YZwNFqFHzKz1KvOZyY79SyXKj6a&format=json&restrict_begin=2020-01-01&restrict_end=2020-01-02&perspective=interval&resolution_time=day&restrict_kind=productivity&device_type=offline')
+	var today = getTime();//select today's date and convert it to a format the API can read
+	axios.get(`https://www.rescuetime.com/anapi/data?key=B63lvEkh_mK25YZwNFqFHzKz1KvOZyY79SyXKj6a&format=json&${today}&perspective=interval&resolution_time=hour&restrict_kind=productivity`)
   .then(response => {
-    //console.log(response.data);// instead of logging, process the data and display it
-		APIparse(response);
-		console.log(productivityObj);
+    console.log(response.data);// instead of logging, process the data and display it
+		//APIparse(response);
+		//console.log(productivityObj);
   })
   .catch(error => {
     console.log(error);
   });}
 
+	rTimeTest();
+
 	const testResponse = {"data":
-		{"rows":
-			[ [ '2020-01-01T00:00:00', 19620, 1, -2 ],
-				[ '2020-01-01T00:00:00', 2348, 1, 0 ],
-				[ '2020-01-01T00:00:00', 1180, 1, 2 ],
-				[ '2020-01-01T00:00:00', 681, 1, 1 ],
-				[ '2020-01-01T00:00:00', 239, 1, -1 ],
-				[ '2020-01-02T00:00:00', 16283, 1, -2 ],
-				[ '2020-01-02T00:00:00', 2393, 1, 1 ],
-				[ '2020-01-02T00:00:00', 2213, 1, 2 ],
-				[ '2020-01-02T00:00:00', 1242, 1, 0 ],
-				[ '2020-01-02T00:00:00', 487, 1, -1 ]
-			]
+		{rows: [
+    [ '2020-03-11T00:00:00', 59, 1, 1 ],
+    [ '2020-03-11T00:00:00', 49, 1, 0 ],
+    [ '2020-03-11T00:00:00', 15, 1, -2 ],
+    [ '2020-03-11T07:00:00', 357, 1, -2 ],
+    [ '2020-03-11T07:00:00', 320, 1, 0 ],
+    [ '2020-03-11T07:00:00', 58, 1, -1 ],
+    [ '2020-03-11T07:00:00', 19, 1, 1 ],
+    [ '2020-03-11T08:00:00', 1452, 1, -2 ],
+    [ '2020-03-11T10:00:00', 67, 1, -2 ],
+    [ '2020-03-11T11:00:00', 1013, 1, -2 ],
+    [ '2020-03-11T12:00:00', 1303, 1, -2 ],
+    [ '2020-03-11T12:00:00', 915, 1, 0 ],
+    [ '2020-03-11T12:00:00', 215, 1, 2 ],
+    [ '2020-03-11T12:00:00', 116, 1, 1 ],
+    [ '2020-03-11T12:00:00', 105, 1, -1 ],
+    [ '2020-03-11T13:00:00', 1432, 1, 0 ],
+    [ '2020-03-11T13:00:00', 1282, 1, -2 ],
+    [ '2020-03-11T13:00:00', 293, 1, 2 ],
+    [ '2020-03-11T13:00:00', 73, 1, -1 ],
+    [ '2020-03-11T13:00:00', 52, 1, 1 ],
+    [ '2020-03-11T14:00:00', 2169, 1, 0 ],
+    [ '2020-03-11T14:00:00', 339, 1, 1 ],
+    [ '2020-03-11T14:00:00', 206, 1, -1 ],
+    [ '2020-03-11T14:00:00', 65, 1, 2 ],
+    [ '2020-03-11T15:00:00', 2728, 1, 0 ],
+    [ '2020-03-11T15:00:00', 212, 1, -2 ],
+    [ '2020-03-11T15:00:00', 83, 1, 1 ],
+    [ '2020-03-11T15:00:00', 24, 1, 2 ],
+    [ '2020-03-11T16:00:00', 142, 1, -1 ],
+    [ '2020-03-11T16:00:00', 62, 1, 0 ],
+    [ '2020-03-11T16:00:00', 34, 1, 1 ],
+    [ '2020-03-11T17:00:00', 629, 1, -2 ],
+    [ '2020-03-11T17:00:00', 30, 1, 0 ],
+    [ '2020-03-11T17:00:00', 13, 1, 1 ],
+    [ '2020-03-11T17:00:00', 3, 1, -1 ],
+  ]
 		}
 	}
 
 function APIparse(response) {
 	var {rows:row} = response.data;
-//	console.log(row);
-	for (var i = 0; i + 1 < row.length; i++) {
-		switch(row[i][3]) {
-			case -2:
-			productivityObj["Very Unproductive"] += row[i][1];
-			break;
-			case -1:
-			productivityObj["Unproductive"] += row[i][1];
-			break;
-			case 0:
-			productivityObj["Neutral"] += row[i][1];
-			break;
-			case 1:
-			productivityObj["Productive"] += row[i][1];
-			break;
-			case 2:
-			productivityObj["Very Productive"] += row[i][1];
-			break;
-		}
-	};
-	console.log(productivityObj);
-}
 
+//	console.log(row);
+	var day = {};
+	for (var i = 0; i < row.length; i++) {
+		var hour = row[i][0].slice(11, 13); //pick the hour out of the
+		//console.log(hour);
+
+switch(row[i][3]) {
+	case -2:
+	var prodLevel = "Very Unproductive";
+	break;
+	case -1:
+	var prodLevel = "Unproductive";
+	break;
+	case 0:
+	var prodLevel = "Neutral";
+	break;
+	case 1:
+	var prodLevel = "Productive";
+	break;
+	case 2:
+	var prodLevel = "Very Productive";
+	break;
+	}
+if (!day[hour]) {
+	day[hour] = {};
+	}
+if (day[hour][prodLevel]) {
+	day[hour][prodLevel] += row[i][1];
+} else {
+	day[hour][prodLevel] = row[i][1]
+}
+//console.log(hour);
+//console.log(day[hour]);
+	};
+	console.log(day);
+}
+APIparse(testResponse);
 
 module.exports = app;

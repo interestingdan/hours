2ea0f0f72a388f66d8b2b6f0bf88b2030f650cf0
Aaron Matthews <dan.interesting@gmail.com>
2020-04-08 18:08:41 +1000

    -carrotstick working

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/',
 app.use(express.static(__dirname+ '/public/'));
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
-mongoose.connect(process.env.MONGO_URI, {
+/*mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true });
 var db = mongoose.connection;
@@ -260,6 +260,57 @@ const userCarrotStick = [
 	}
 ]; // replace this with object in user instance
 
+const testResponse = {"date": "2020-01-01",
+"data":
+{rows: [
+	[ '2020-03-11T00:00:00', 59, 1, 1 ],
+	[ '2020-03-11T00:00:00', 49, 1, 0 ],
+	[ '2020-03-11T00:00:00', 15, 1, -2 ],
+
+	[ '2020-03-11T07:00:00', 357, 1, -2 ],
+	[ '2020-03-11T07:00:00', 320, 1, 0 ],
+	[ '2020-03-11T07:00:00', 58, 1, -1 ],
+	[ '2020-03-11T07:00:00', 19, 1, 1 ],
+
+	[ '2020-03-11T08:00:00', 1452, 1, -2 ],
+	[ '2020-03-11T10:00:00', 67, 1, -2 ],
+
+	[ '2020-03-11T11:00:00', 1013, 1, -2 ],
+
+	[ '2020-03-11T12:00:00', 1303, 1, -2 ],
+	[ '2020-03-11T12:00:00', 915, 1, 0 ],
+	[ '2020-03-11T12:00:00', 215, 1, 2 ],
+	[ '2020-03-11T12:00:00', 116, 1, 1 ],
+	[ '2020-03-11T12:00:00', 105, 1, -1 ],
+
+	[ '2020-03-11T13:00:00', 1432, 1, 0 ],
+	[ '2020-03-11T13:00:00', 1282, 1, -2 ],
+	[ '2020-03-11T13:00:00', 293, 1, 2 ],
+	[ '2020-03-11T13:00:00', 73, 1, -1 ],
+	[ '2020-03-11T13:00:00', 52, 1, 1 ],
+
+	[ '2020-03-11T14:00:00', 2169, 1, 0 ],
+	[ '2020-03-11T14:00:00', 339, 1, 1 ],
+	[ '2020-03-11T14:00:00', 206, 1, -1 ],
+	[ '2020-03-11T14:00:00', 65, 1, 2 ],
+
+	[ '2020-03-11T15:00:00', 2728, 1, 0 ],
+	[ '2020-03-11T15:00:00', 212, 1, -2 ],
+	[ '2020-03-11T15:00:00', 83, 1, 1 ],
+	[ '2020-03-11T15:00:00', 24, 1, 2 ],
+
+	[ '2020-03-11T16:00:00', 142, 1, -1 ],
+	[ '2020-03-11T16:00:00', 62, 1, 0 ],
+	[ '2020-03-11T16:00:00', 34, 1, 1 ],
+
+	[ '2020-03-11T17:00:00', 629, 1, -2 ],
+	[ '2020-03-11T17:00:00', 30, 1, 0 ],
+	[ '2020-03-11T17:00:00', 13, 1, 1 ],
+	[ '2020-03-11T17:00:00', 3, 1, -1 ],
+]
+}
+}
+
 function parseTime(dateObj){
 	//var date = dateObj.subtract(1, 'days');
 	var date =dateObj
@@ -324,14 +375,14 @@ switch(row[i][3]) {  //pick the productivity level from the row an
 	break;
 	}
 	if (!day.hourArray[hourNumb]) {
-		console.log(day.hourArray[hourNumb-1]);
+		//console.log(day.hourArray[hourNumb-1]);
 		day.hourArray[hourNumb] = {hourStarts: hourNumb, productivity:{}};
 	}
 	day.hourArray[hourNumb]["productivity"][prodLevel] = row[i][1]
 
 //console.log(day[hour]);
 	};
-	console.log(day);
+	//console.log(day);
 	carrotStick(day);
 	//console.log(day.hour);
 	//console.log('Parsed:');
@@ -341,84 +392,44 @@ switch(row[i][3]) {  //pick the productivity level from the row an
 
 function carrotStick(dayRecord) {
 	var dayProcessed = {
-	daySticks: 0,
-	dayCarrots: 0
+		dayStick: 0,
+		dayCarrot: 0
 	}
-	for (var i = 0; i < 24; i++) {
-		if (dayRecord.hour[i]) {//dayRecord may be empty for this hour
-			loopableCarrotStick = userCarrotStick[i].entries;
-			for (var j = 0; j < loopableCarrotStick.length; j++) {
-				loopableCarrotStick[j]
+	//console.log(dayRecord);
+	for (var i = 0; i < dayRecord.hourArray.length - 1; i++) {
+		if (dayRecord.hourArray[i]) {//dayRecord may be empty for this hour
+			var loopableHour = Object.entries(dayRecord.hourArray[i].productivity);
+			//console.log(loopableHour);
+			for (var j = 0; j < loopableHour.length; j++) {
+				var hourRowProdLevel = loopableHour[j][0];//returns an array, ["Prodlevel", Number]
+				//console.log(hourRowProdLevel);
+				var hourRowTimeAmount = loopableHour[j][1];
+				//console.log(hourRowTimeAmount);
+				if (userCarrotStick[i].byProd[hourRowProdLevel] < 0) {
+					dayProcessed.dayStick += hourRowTimeAmount * userCarrotStick[i].byProd[hourRowProdLevel];
+				} else if (userCarrotStick[i].byProd[hourRowProdLevel] > 0) {
+					dayProcessed.dayCarrot += hourRowTimeAmount * userCarrotStick[i].byProd[hourRowProdLevel];
+				}
 			}
 		}
-			if (userCarrotStick[i][] =< 0) {
-				daySticks += userCarrotStick[i].VUnp
-			}
+
 			/*VUnp
 			Unpr
 			Neut
 			Prod
 			VPro*/
 		}
+	console.log(dayProcessed)
 	}
-}
 
-//APIparse(testResponse);
-//logYesterday();
+
+APIparse(testResponse);
+//logYesterday(testResponse);
 //getTime();
 //searchAll();
 
 
-/*const testResponse = {"date": "2020-01-01",
-"data":
-{rows: [
-	[ '2020-03-11T00:00:00', 59, 1, 1 ],
-	[ '2020-03-11T00:00:00', 49, 1, 0 ],
-	[ '2020-03-11T00:00:00', 15, 1, -2 ],
 
-	[ '2020-03-11T07:00:00', 357, 1, -2 ],
-	[ '2020-03-11T07:00:00', 320, 1, 0 ],
-	[ '2020-03-11T07:00:00', 58, 1, -1 ],
-	[ '2020-03-11T07:00:00', 19, 1, 1 ],
-
-	[ '2020-03-11T08:00:00', 1452, 1, -2 ],
-	[ '2020-03-11T10:00:00', 67, 1, -2 ],
-
-	[ '2020-03-11T11:00:00', 1013, 1, -2 ],
-
-	[ '2020-03-11T12:00:00', 1303, 1, -2 ],
-	[ '2020-03-11T12:00:00', 915, 1, 0 ],
-	[ '2020-03-11T12:00:00', 215, 1, 2 ],
-	[ '2020-03-11T12:00:00', 116, 1, 1 ],
-	[ '2020-03-11T12:00:00', 105, 1, -1 ],
-
-	[ '2020-03-11T13:00:00', 1432, 1, 0 ],
-	[ '2020-03-11T13:00:00', 1282, 1, -2 ],
-	[ '2020-03-11T13:00:00', 293, 1, 2 ],
-	[ '2020-03-11T13:00:00', 73, 1, -1 ],
-	[ '2020-03-11T13:00:00', 52, 1, 1 ],
-
-	[ '2020-03-11T14:00:00', 2169, 1, 0 ],
-	[ '2020-03-11T14:00:00', 339, 1, 1 ],
-	[ '2020-03-11T14:00:00', 206, 1, -1 ],
-	[ '2020-03-11T14:00:00', 65, 1, 2 ],
-
-	[ '2020-03-11T15:00:00', 2728, 1, 0 ],
-	[ '2020-03-11T15:00:00', 212, 1, -2 ],
-	[ '2020-03-11T15:00:00', 83, 1, 1 ],
-	[ '2020-03-11T15:00:00', 24, 1, 2 ],
-
-	[ '2020-03-11T16:00:00', 142, 1, -1 ],
-	[ '2020-03-11T16:00:00', 62, 1, 0 ],
-	[ '2020-03-11T16:00:00', 34, 1, 1 ],
-
-	[ '2020-03-11T17:00:00', 629, 1, -2 ],
-	[ '2020-03-11T17:00:00', 30, 1, 0 ],
-	[ '2020-03-11T17:00:00', 13, 1, 1 ],
-	[ '2020-03-11T17:00:00', 3, 1, -1 ],
-]
-}
-}
 
 	function checkForDupes(response) {
 		var {rows:row} = response.data;
@@ -438,5 +449,5 @@ function carrotStick(dayRecord) {
 			console.log(units)
 		}
 	}
-*/
+
 module.exports = app;
